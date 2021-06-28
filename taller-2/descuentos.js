633cf14537d1982fb1e205a0b7300fe077f8ff5d
Aldo-Salas <salas_rdz@outlook.es>
2021-06-28 03:12:22 -0500

    Añadiendo funcion para aplicar cupones de descuento

--- taller-2/descuentos.js
+++ taller-2/descuentos.js
@@ -1,12 +1,48 @@
-const precioOriginal = 120;
-const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
-  console.log(`El precio es: $${precio}`);
-  console.log(`El precio con ${descuento}% de descuento es: $${precioConDescuento}`);
+  return precioConDescuento;
 }
 
+function calcularDescuento() {
+  const inputPrice = document.getElementById('InputPrice');
+  const priceValue = inputPrice.value;
 
+  const inputDiscount = document.getElementById('InputDiscount');
+  const discountValue = inputDiscount.value;
+
+  const inputCoupon = document.getElementById('InputCoupon');
+  const couponValue = inputCoupon.value;
+
+  const coupons = [
+    {
+      name: "Descuento",
+      discount: 15,
+    },
+    {
+      name: "Platzi",
+      discount: 30,
+    },
+    {
+      name: "100OFF",
+      discount: 100,
+    },
+  ];
+
+  const isCouponValid = coupon => {
+    return coupon.name === couponValue;
+  }
+
+  const userCoupon = coupons.find(isCouponValid);
+
+  if (!userCoupon) {
+    alert(`Cupón ${couponValue} no es válido`);
+  } else {
+    const descuento = userCoupon.discount;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const result = document.getElementById('ResultPrice');
+    result.innerText = `El precio con descuento es: $${precioConDescuento}`;
+  }
+}
